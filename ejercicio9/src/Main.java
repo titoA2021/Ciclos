a622b5349f6c31b534102accfb8e4899482b6973
Tito Alejandro Zapata <titoalejandro97@gmail.com>
2022-11-29 19:48:30 -0500

    show the numbers from 100 to 0 step by 7

---
+++ ejercicio9/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Escribir todos los numeros del 100 al 0 de 7 en 7
+         */
+
+        for (int i=100; i >= 0; i -=7){
+            System.out.println(i);
+        }
+    }
+}
